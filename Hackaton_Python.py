208bddbeb937db0a0c38594510de932831a608a3
Rafael Barragan <rafael.barragan@outlook.com>
2019-09-08 03:29:35 -0500

    -- Se agrega la clasificacion de tipo de cuenta 0: Cobros, 1: Pagos y 2: Transaccionable

--- Hackaton_Python.py
+++ Hackaton_Python.py
@@ -48,6 +48,101 @@ mapSIGNO = {'+':1, '-':0}
 ## Reasigno los valores de entrada y salida de las operaciones
 dfClientes.loc[:,'idSIGNO'] = dfClientes.loc[:,'SIGNO'].map(mapSIGNO)
 
+## Identifico que tipo de cuenta es
+
+## Debido a que asignamos valores de 1 y 0 utilizamos el promedio para identificar si pertenece a
+## ambos tipos de transaccionalidad.
+dfTipCta = dfClientes.groupby(['CUENTA'], as_index = False).agg({'idSIGNO':'mean'})
+## Para reclasificar si es de ambos tipos todo aquello que no sea 1 o 0 se clasifica como transaccionable
+## 1 Pagos : 0 Cobros
+dfTipCta.loc[(dfTipCta['idSIGNO']>0) & (dfTipCta['idSIGNO']<1), 'idSIGNO'] = 2
+
+
+#######################################################
+####################### PRUEBAS #######################
+#######################################################  
+
+
+## Pivoteo la informacion
+UnPivotClien = dfClieProd.melt(id_vars = ['CLIENTE','GRUPO','ALTA_CLIENTE','FH_NACIMIENTO','SEGMENTO','CD_GIRO','ZIP_CODE','NB_ACTIVIDAD'], var_name = 'TipoProd', value_name= 'PromMont')
+
+UnPivotClien = dfClieProd.loc[:,['CLIENTE','GRUPO','ALTA_CLIENTE','FH_NACIMIENTO','SEGMENTO','CD_GIRO','ZIP_CODE','NB_ACTIVIDAD','AP25',	'AP26',	'AP27',	'AP28',	'AP29',	'AP30',	'AP31',	'AP32',	'AP33',	'AP34',	'AP35',	'AP36',	'CV25',	'CV26',	'CV27',	'CV28',	'CV29',	'CV30',	'CV31',	'CV32',	'CV33',	'CV34',	'CV35',	'CV36',	'CX25',	'CX26',	'CX27',	'CX28',	'CX29',	'CX30',	'CX31',	'CX32',	'CX33',	'CX34',	'CX35',	'CX36',	'PP25',	'PP26',	'PP27',	'PP28',	'PP29',	'PP30',	'PP31',	'PP32',	'PP33',	'PP34',	'PP35',	'PP36',	'CT25',	'CT26',	'CT27',	'CT28',	'CT29',	'CT30',	'CT31',	'CT32',	'CT33',	'CT34',	'CT35',	'CT36',	'PE25',	'PE26',	'PE27',	'PE28',	'PE29',	'PE30',	'PE31',	'PE32',	'PE33',	'PE34',	'PE35',	'PE36',	'VE25',	'VE26',	'VE27',	'VE28',	'VE29',	'VE30',	'VE31',	'VE32',	'VE33',	'VE34',	'VE35',	'VE36',	'SI25',	'SI26',	'SI27',	'SI28',	'SI29',	'SI30',	'SI31',	'SI32',	'SI33',	'SI34',	'SI35',	'SI36',	'PN25',	'PN26',	'PN27',	'PN28',	'PN29',	'PN30',	'PN31',	'PN32',	'PN33',	'PN34',	'PN35',	'PN36',	'VN25',	'VN26',	'VN27',	'VN28',	'VN29',	'VN30',	'VN31',	'VN32',	'VN33',	'VN34',	'VN35',	'VN36']].melt(id_vars = ['CLIENTE','GRUPO','ALTA_CLIENTE','FH_NACIMIENTO','SEGMENTO','CD_GIRO','ZIP_CODE','NB_ACTIVIDAD'], var_name = 'TipoProd', value_name= 'PromMont')
+
+#xx = dfClientes.iloc[:10,:1].head()
+
+xx = dfTrain.head(5000)
+xx['CodSIGNO'] = xx.loc[:, 'SIGNO'].map(mapSIGNO)
+#xx['TipoCuenta'] = xx['CUENTA'] + xx['MONEDA']
+#len(xx.TipoCuenta.unique())
+#len(xx.CUENTA.unique())
+tmpDF = xx.groupby(['CUENTA'], as_index = False).agg({'CodSIGNO':'mean'})
+tmpDF.loc[(tmpDF['CodSIGNO']>0) & (tmpDF['CodSIGNO']<1), 'CodSIGNO'] = 2
+
+
+for idx, i in tmpDF.iterrows():
+    print(idx, i)
+
+
+Cta = xx['CUENTA'].values
+Sgn = xx['CodSIGNO'].values
+
+dicN = dict(zip(Cta, Sgn))
+
+#a = np.unique(xx[['CUENTA','MONEDA']].values)
+
+TipoCuenta = xx.groupby(['CUENTA','SIGNO']).count() 
+#TipoCuenta2 = xx['CUENTA','SIGNO'].groupby(['CUENTA','SIGNO']).count()
+
+IdxTipCta = []
+tempVal = ''
+
+idxV = TipoCuenta.index.values
+
+idxV = list(idxV)
+aux = ''
+diccionario = {}
+
+for i in range(0,len(idxV)):
+    if aux == idxV[i][0]:
+        print(idxV[i][0], aux)
+        diccionario[aux] = 2
+    else:
+        if idxV[i][1] == '+':
+            diccionario[aux] = 1
+        else:
+            diccionario[aux] = 0
+    aux = idxV[i][0]
+    
+for idx in range(0,len(idxV)-1100):
+#    print(idx)
+    a = idx
+    if idxV[idx] == idxV[idx-1]:
+        print(idxV[idx][0], idxV[idx][1])
+
+    print(idx, '   ' , i)
+    
+
+for idx, i in TipoCuenta['TipoCuenta'].iteritems():
+    print(TipoCuenta[idx])
+    IdxTipCta.append(TipoCuenta[idx][0])
+    
+#    tempVal = idx
+#    if tempVal == idx:
+#        IdxTipCta.append(idx,3)
+#    elif 
+
+dd = TipoCuenta.index.values
+
+for i in len(TipoCuenta):
+    if TipoCuenta.iloc[i,:] == TipoCuenta.iloc[i+1,:]:
+        IdxTipCta.append[TipoCuenta.iloc[i,:]]
+
+aaaa = TipoCuenta.groupby('FH_OPERACION').count()
+
+#TipoCuenta = xx.groupby(['CUENTA','COD_TRNFIMS','MONEDA','SIGNO']).count() 
+
+UnPivotClien.groupby('CLIENTE')['PromMont'].sum()
+
 #### Seccion de pruebas
 #dfTrain.head()
 #
